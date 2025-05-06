0928e339ca351b3a854896ad650477a1c1bd2c57
Emanuel <emanuelflavio1402@gmail.com>
2025-05-06 15:40:22 -0300

    feat: Create Entity, Repository, Controller, Services and Migrations for Category

---
+++ src/main/java/dev/emanuel/movieflix/Controller/CategoryController.java
@@ -0,0 +1,43 @@
+package dev.emanuel.movieflix.Controller;
+
+import dev.emanuel.movieflix.Service.CategoryService;
+import dev.emanuel.movieflix.entity.Category;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/movieflix/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+
+    private final CategoryService categoryService;
+
+    @GetMapping()
+    public List<Category> getAllCategories() {
+        return categoryService.findAll();
+    }
+
+    @PostMapping()
+    public Category saveCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @GetMapping("/{id}")
+    public Category getByCategoriesId(@PathVariable Long id) {
+        Optional<Category> optCategory = categoryService.findById(id);
+        if (optCategory.isPresent()) {
+            return optCategory.get();
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteByCategoryId(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+    }
+
+}
