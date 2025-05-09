f9e19c6593783ec2bd3abd898a07b9dbcffb8e86
Emanuel <emanuelflavio1402@gmail.com>
2025-05-08 21:05:47 -0300

    feat: create entity and repository for movie

---
+++ src/main/java/dev/emanuel/movieflix/Repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package dev.emanuel.movieflix.Repository;
+
+import dev.emanuel.movieflix.Entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
